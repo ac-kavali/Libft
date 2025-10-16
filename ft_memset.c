0205826cecb573fb377a5e60ae96276493ab65af
achahi <ahmadachahi400@gmail.com>
2025-10-16 18:24:15 -0400

    ----------

--- ft_memset.c
+++ ft_memset.c
@@ -1,3 +1,28 @@
-#include <unistd.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: achahi <achahi@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/16 10:13:50 by achahi            #+#    #+#             */
+/*   Updated: 2025/10/16 17:58:14 by kali             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "libft.h" 
 
+void    *ft_memset(void *ptr, int v, size_t s)
+{
+    unsigned char *arr;
+    size_t       i;
+
+    arr = (unsigned char *)ptr;
+    i = 0;
+    while (i < s)
+    {
+        arr[i] = (unsigned char)v; //arr is type unsigned char then you should cast the v.
+        i++;
+    }
+    return (ptr);
+}
---
+++ readme.md
@@ -0,0 +1,14 @@
+#the sizeof explaine
+    - unsigned integer type used to represent sizes of objects in memory.
+    - It’s guaranteed to be big enough to store the size of the largest possible object your system can handle.
+    - in sumary it used to specify the size of an objects because it can safly hold ony object size and cannot be negative.
+
+#explain the memset use 
+    it a function that assig a value to  a block of memory byte per byte and not type, used to inicialize memory bytes with 0 and som other uses 
+
+#take int 1 and char 'A' like an example
+    if you entred a 1 like a parametre in the memset funtion to fill an array with it will
+    fill each byte with 00000001 = 0x00(hex) then you will get the 16843009 in each int size
+    the resault of the test will be 
+    16843009, 16843009, 16843009, 16843009, 
+    AAAA 
