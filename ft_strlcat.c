690d0fbb8655ffe028d42cf68dffc72a4c7f6b40
achahi <ahmadachahi400@gmail.com>
2025-10-21 18:29:41 -0400

    <200e>

---
+++ ft_strlcat.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: achahi <achahi@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/21 16:27:22 by achahi            #+#    #+#             */
+/*   Updated: 2025/10/21 18:23:44 by achahi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t  ft_strlcat(char *dst, const char *src, size_t size)
+{
+    size_t  src_len;
+    size_t  dst_len;
+    size_t i;
+    
+    src_len = ft_strlen(src);
+    dst_len = ft_strlen(dst);
+    i = 0;
+    if (size <= dst_len)
+        return size + src_len;
+    while (src[i] && dst_len + i < size - 1)
+    {
+        dst[dst_len + i] = src[i];
+        i++;
+    }
+    dst[dst_len + i] = '\0';
+    return (src_len + dst_len);
+}
--- readme.md
+++ readme.md
@@ -102,3 +102,34 @@ However, if you check manually and return before using `dest`, that avoids the c
 ```
 `strlcpy` will **never copy more than the actual length of `src`**, even if the `size` argument allows more space.
 
+
+####strlcat:
+The function **`strlcat`** is used to **concatenate (append)** one string to another, but **safely**, by preventing buffer overflows.
+prototype:
+```c
+size_t strlcat(char *dst, const char *src, size_t size);
+```
+- The loop of `strlcat` continues **until one of these two condition is met:
+```c
+while (src[i] && (dst_len + i) < (size - 1))
+```  
+- It completely depends on the size given in the arguments — if the user sets a size larger than the destination buffer, an overflow will occur.
+
+```c
+char dest[5] = "abc";
+ft_strlcat(dest, "defghijkl", 20);  // ❌ dangerous
+```
+
+- Remember to use : 
+```c 
+dst[dst_len + i] = '\0'; // ❌ dst[size-1] = '\0'
+```
+because this will causes undifined behaviors if the src + dest < size.
+- your return the `src_len`+`dst_len` to compare the it with the final `dest` to know if the append currupted.
+
+- Optimize the code with the case if the `size < dst_len` you it should return `size+src_len`
+```c
+if (size <= dst_len)
+    return size + src_len;
+```
+this avoid writting outside the buffer and the return value to used to tell the total size needed to apped next time. 
