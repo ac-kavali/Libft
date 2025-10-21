2e83c02602d3ee1e595401d5e1e75bf2eeca90fc
Ahmed Chahi <achahi@c3r8p6.1337.ma>
2025-10-21 17:54:37 +0100

    libft

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,24 +6,26 @@
 /*   By: achahi <achahi@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 15:03:34 by achahi            #+#    #+#             */
-/*   Updated: 2025/10/21 17:02:21 by achahi           ###   ########.fr       */
+/*   Updated: 2025/10/21 17:54:09 by achahi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strlcpy(char dest, const char src, size_t size)
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
-	size_t	src_len;
-	size_t	dest_len;
 	size_t	i;
-	
-	src_len = ft_strlen(src);
-	dest_len = ft_strlen(dst);
-	i = 0;
-	if (dest_len == 0) || (size == 0))
-		return (src_len);
-	while((i <  size) && ())
-	
+	size_t	src_len;
 
+	src_len = ft_strlen(src);
+	if (size == 0)
+		return (src_len);
+	i = 0;
+	while (i < size - 1 && src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (src_len);
 }
