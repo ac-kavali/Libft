187742f7b7ab9218a7319c107f3fdc2af90290ab
Ahmed Chahi <achahi@c2r1p12.1337.ma>
2025-10-28 09:55:15 +0100

    libft

---
+++ ft_strmapi.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: achahi <achahi@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/28 09:21:49 by achahi            #+#    #+#             */
+/*   Updated: 2025/10/28 09:22:27 by achahi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	size_t	i;
+	size_t	len;
+	char	*new_s;
+
+	if (!s || !f)
+		return (NULL);
+	len = ft_strlen(s);
+	new_s = (char *)malloc(len + 1);
+	if (!new_s)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		new_s[i] = f(i, s[i]);
+		i++;
+	}
+	new_s[i] = '\0';
+	return (new_s);
+}
